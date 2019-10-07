5a2b65da02e0fd305c4752d1d9a64b8396e92152
EVERYDAYHERO2000 <ilyaflare@gmail.com>
2019-10-07 21:39:58 +0300

    fix

--- source/app/main.js
+++ source/app/main.js
@@ -2,8 +2,8 @@
 $(function(){
   
   var leafletMap = L.map('map').setView([55.751244, 37.618423], 9);
-  L.tileLayer("https://{s}.basemaps.cartocdn.com/rastertiles/voyager_labels_under/{z}/{x}/{y}.png")
-  //L.tileLayer("https://{s}.tiles.mapbox.com/v4/mapbox.dark/{z}/{x}/{y}@2x.png?access_token=pk.eyJ1IjoiZ2xlYWZsZXQiLCJhIjoiY2lxdWxoODl0MDA0M2h4bTNlZ2I1Z3gycyJ9.vrEWCC2nwsGfAYKZ7c4HZA")
+  //L.tileLayer("https://{s}.basemaps.cartocdn.com/rastertiles/voyager_labels_under/{z}/{x}/{y}.png")
+  L.tileLayer("https://{s}.tiles.mapbox.com/v4/mapbox.dark/{z}/{x}/{y}@2x.png?access_token=pk.eyJ1IjoiZ2xlYWZsZXQiLCJhIjoiY2lxdWxoODl0MDA0M2h4bTNlZ2I1Z3gycyJ9.vrEWCC2nwsGfAYKZ7c4HZA")
     .addTo(leafletMap);
   
   window.map = leafletMap;
