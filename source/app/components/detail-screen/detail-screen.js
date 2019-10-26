4256321bf31433951deba9e9f220f3afe9cf42b7
EVERYDAYHERO2000 <ilyaflare@gmail.com>
2019-10-26 17:37:49 +0300

    Fix

--- source/app/components/detail-screen/detail-screen.js
+++ source/app/components/detail-screen/detail-screen.js
@@ -5,20 +5,49 @@ __.detailScreen = function (data){
   let _address = contentItem('Адрес', data.address.name, function(v){ return v });
   let _city = contentItem('Ближайший город', data.city.closest.name, function(v){ return `${v} в ${data.city.distance}км` });
   let _moscow = contentItem('Расстояние от Москвы', data.moscow.distance, function(v){ return `${v}км` });
-  let _car = contentItem('На автомобиле до Москвы', data.car.distance, function(v){ return `${v}км, ${data.car.time.h}ч ${data.car.time.m}мин без учета пробок` });
-  let _railroad = contentItem('Ближайшая ж/д станция', data.railroad.closest.name, function(v){ return `${v} в ${data.railroad.distance}км, до Москвы ${data.railroad.closest.time.h}ч ${data.railroad.closest.time.m}мин` });
-  let _eco = contentItem('Экология', data.eco.closest.name, function(v){ return `${data.eco.distance}км до ${v}`});
+  let _car = contentItem('На автомобиле до Москвы', data.car.distance, function(v){ return `${v}км<br> ${data.car.time.h}ч ${data.car.time.m}мин без учета пробок` });
+  let _railroad = contentItem('Ближайшая ж/д станция', data.railroad.closest.name, function(v){ return `${v} в ${data.railroad.distance}км<br> до Москвы ${data.railroad.closest.time.h}ч ${data.railroad.closest.time.m}мин` });
+  let _eco = contentItem('Ближайший источник загрязнения', data.eco.closest.name, function(v){ 
+    let description = (data.eco.closest.description) ? `<br>${data.eco.closest.description}` : '';
+    return `${data.eco.distance}км до ${v} ${description}`
+  });
+  let _price = (function(price){
+    
+    let from = (price.from) ? `от ${__.cost({value:price.from})[0]}руб. ` : '',
+        to = (price.to) ? `<br>до ${__.cost({value:price.to})[0]}руб.` : '',
+        p = from + to;
+    
+    return (p) ? contentItem('Цена', p, function(v){ return v }) : '';
+    
+    
+  })(data.price);
+  let _developer = contentItem('Застройщик', data.developer, function(v){ return v; });
+  let _site = contentItem('Сайт', data.site, function(v){ return `<a href="${v}">${v}</a>`; });
   
   let container_tpl = `
 <h1>${_type} <span>${data.name}</span></h1>
 <div class="panel__content content">
-  ${_class}
-  ${_address}
-  ${_city}
-  ${_moscow}
-  ${_car}
-  ${_railroad}
-  ${_eco}
+  <section>
+    ${_class}
+    ${_price}
+    ${_developer}
+    ${_site}
+    ${_address}
+    ${_moscow}
+  </section>
+  <section>
+    <h2>Транспорт</h2>
+    ${_car}
+    ${_railroad}
+  </section>
+  <section>
+    <h2>Инфраструктура</h2>
+    ${_city}
+  </section>
+  <section>
+    <h2>Экология</h2>
+    ${_eco}
+  </section>
 </div>
   `; 
   
