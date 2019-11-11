7b17f18f28446ca7e3915ede3e884892e9194f94
EVERYDAYHERO2000 <ilyaflare@gmail.com>
2019-11-11 16:46:18 +0300

    Fix

--- source/app/fs/map-tiles/map-tiles.js
+++ source/app/fs/map-tiles/map-tiles.js
@@ -1,4 +1,10 @@
-__.fs.mapTiles = {
-      simple : 'https://{s}.basemaps.cartocdn.com/rastertiles/voyager_labels_under/{z}/{x}/{y}.png',
-      dark : 'https://{s}.tiles.mapbox.com/v4/mapbox.dark/{z}/{x}/{y}@2x.png?access_token=pk.eyJ1IjoiZ2xlYWZsZXQiLCJhIjoiY2lxdWxoODl0MDA0M2h4bTNlZ2I1Z3gycyJ9.vrEWCC2nwsGfAYKZ7c4HZA'
-    }  
+__.fs.mapTiles = (function(){
+  
+  let format = (__.fs.browserDetect().browserName == 'chrome') ? 'webp' : 'png';
+  
+  return {
+      simple : `https://{s}.basemaps.cartocdn.com/rastertiles/voyager_labels_under/{z}/{x}/{y}.${format}`,
+      dark : `https://{s}.tiles.mapbox.com/v4/mapbox.dark/{z}/{x}/{y}@2x.${format}?access_token=pk.eyJ1IjoiZ2xlYWZsZXQiLCJhIjoiY2lxdWxoODl0MDA0M2h4bTNlZ2I1Z3gycyJ9.vrEWCC2nwsGfAYKZ7c4HZA`
+    } 
+  
+})();
