75fb41fbc975be8778609215086e8a0da1293137
EVERYDAYHERO2000 <ilyaflare@gmail.com>
2019-11-21 18:26:55 +0300

    Fix

--- gulp/title-generator.js
+++ gulp/title-generator.js
@@ -1,8 +1,8 @@
 let titleGenerator = function(data){
-  
+  let type = (data.type) ? data.type : 'КП';
   let title = '';
   
-  title = `${data.type} ${data.name} в ${data.moscow.distance} км от Москвы, вблизи города ${data.city.closest.name}.` 
+  title = `${type} ${data.name} в ${data.moscow.distance} км от Москвы, вблизи города ${data.city.closest.name}.` 
   
   return title;
 }
