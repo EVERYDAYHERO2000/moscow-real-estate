ab5ddc6e718ebfd498c7612ae0bf16a5d2632be2
EVERYDAYHERO2000 <ilyaflare@gmail.com>
2019-11-28 13:51:44 +0300

    Fix

--- gulp/write-files.js
+++ gulp/write-files.js
@@ -143,7 +143,7 @@ let writeFiles = function(data, allPages){
       process.stdout.write(`\n`);
     }
     
-    sitemap = `<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1">${sitemap}</urlset>`;
+    sitemap = `<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">${sitemap}</urlset>`;
     
     
     
