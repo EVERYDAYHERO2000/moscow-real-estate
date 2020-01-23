b93e952b1c349b92e2e45860312fcd32afddbbed
EVERYDAYHERO2000 <ilyaflare@gmail.com>
2020-01-23 18:34:41 +0300

    Fix

--- source/app/core/map/map.js
+++ source/app/core/map/map.js
@@ -23,8 +23,6 @@ __.core.$map = function(){
     
     var leafletMap = L.map(id).setView([55.751244, 37.618423], 9);
     var tiles = L.tileLayer(t.dark, {attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'});
-    
-    console.log(tiles);  
       
        
       tiles.on('tileload', function (tileEvent) {
