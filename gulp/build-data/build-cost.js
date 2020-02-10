caa04770296aa523326f030bcef6b6a053d9246e
EVERYDAYHERO2000 <ilyaflare@gmail.com>
2020-02-10 16:26:47 +0300

    Fix

--- gulp/build-data/build-cost.js
+++ gulp/build-data/build-cost.js
@@ -4,18 +4,31 @@ const writeWorldData = require('@root/gulp/build-data/fs/write-world-data.js');
 const build = function(){
   
   const cost = require('@data/cost.json').rows;
+  const cost_2 = require('@data/cost_2.json');
   const places = require('@data/places/places.json');
   let result = [],
   count = 1;
     
     _.forEach(cost, function(e){
       
-
-
       let c = {
         id : count,
         point : [+e.longitude, +e.latitude],
         cost : +e.PriceSearch
+      }
+      
+      count++;
+
+      result.push(c);
+      
+    });
+
+    _.forEach(cost_2, function(e){
+      
+      let c = {
+        id : count,
+        point : [+e.lon, +e.lat],
+        cost : +e.price
       }
       
       count++;
